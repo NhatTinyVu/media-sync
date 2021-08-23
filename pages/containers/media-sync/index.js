d4987f3956142f2e950bfa2114eb098024bd81a5
Nhat Vu <nhat.vu@employmenthero.com>
2021-08-23 14:55:30 +0700

    add socketio

--- pages/containers/media-sync/index.js
+++ pages/containers/media-sync/index.js
@@ -1,8 +1,23 @@
+import React, { useEffect } from "react";
+import SocketIOClient from "socket.io-client";
+
 import MediaPlayer from "../../components/media-player";
 import User from "../../components/user";
 import styles from "../../../styles/Home.module.css";
 
 export default function Home() {
+  useEffect(() => {
+    const socket = SocketIOClient.connect(process.env.BASE_URL, {
+      path: "/api/socketio",
+    });
+
+    socket.on("connect", () => {
+      console.log("SOCKET CONNECTED!", socket.id);
+    });
+
+    if (socket) return () => socket.disconnect();
+  }, []);
+
   return (
     <div className={styles.container}>
       <User name="Nhat Vu" />
