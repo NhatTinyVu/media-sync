9640a4b38092498d89664f1c21afa6e5f0078c68
Nhat Vu <nhat.vu@employmenthero.com>
2021-08-28 15:52:44 +0700

    add flip feature

--- pages/containers/media-player/index.js
+++ pages/containers/media-player/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect, useRef } from "react";
-import { Upload } from "antd";
+import { Upload, Switch } from "antd";
 import ReactPlayer from "react-player";
 import axios from "axios";
 
@@ -7,10 +7,23 @@ const { Dragger } = Upload;
 
 const beforeUpload = () => false;
 
+function secondsToHms(d) {
+  d = Number(d);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor((d % 3600) / 60);
+  var s = Math.floor((d % 3600) % 60);
+
+  var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+  var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+  return hDisplay + mDisplay + sDisplay;
+}
+
 const MediaPlayer = ({ isHost, time }) => {
   console.log(isHost);
   const player = useRef(null);
   const [fileURL, setFileURL] = useState(null);
+  const [flip, setFlip] = useState(false);
 
   const handleAddFile = useCallback(({ file }) => {
     setFileURL(URL.createObjectURL(file));
@@ -47,17 +60,30 @@ const MediaPlayer = ({ isHost, time }) => {
         onChange={handleAddFile}
         style={{ padding: "0 16px" }}
       >
-        Kéo video vào đây nhé
+        Bấm chọn hoặc Kéo video vào đây nhé
       </Dragger>
       {fileURL && (
-        <ReactPlayer
-          ref={player}
-          width="100%"
-          height="100%"
-          controls
-          playing
-          url={fileURL}
-        />
+        <>
+          <div style={{ textAlign: "center", padding: 8 }}>
+            <span style={{ fontWeight: "bold" }}>Lật hướng cho video </span>
+            <Switch value={flip} onChange={(newFlip) => setFlip(newFlip)} />
+            <span>
+              {" "}
+              Thời gian mà host đang play:{" "}
+              {time ? secondsToHms(time) : <b>Chưa có host</b>}
+            </span>
+          </div>
+
+          <ReactPlayer
+            ref={player}
+            width="100%"
+            height="100%"
+            controls
+            playing
+            url={fileURL}
+            style={flip ? { transform: "scaleX(-1)" } : undefined}
+          />
+        </>
       )}
     </div>
   );
