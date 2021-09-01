6e131de221245d3d5fde56340c64099011ad479c
Nhat Vu <nhat.vu@employmenthero.com>
2021-09-01 16:53:09 +0700

    fix

--- pages/containers/MediaPlayer.js
+++ pages/containers/MediaPlayer.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useEffect, useRef } from "react";
 import { Switch, Button } from "antd";
-import { PlayCircleOutlined, PauseCircleOutlined } from "@ant-design/icons";
+import { PlayCircleOutlined, SyncOutlined } from "@ant-design/icons";
 import ReactPlayer from "react-player";
 import axios from "axios";
 import { isEmpty, debounce } from "lodash";
@@ -33,7 +33,9 @@ const MediaPlayer = ({
   const player = useRef(null);
   const [delay, setDelay] = useState(0.0);
   const [flip, setFlip] = useState(false);
+  const [syncing, setSyncing] = useState(true);
   const [playingStatus, setPlayingStatus] = useState(true);
+  const [isForcingSync, setIsForcingSync] = useState(false);
 
   const handleUpdateTime = useCallback(async () => {
     const time = player?.current?.getCurrentTime();
@@ -52,14 +54,21 @@ const MediaPlayer = ({
 
   const handleForceSync = useCallback(() => {
     if (!isNaN(player?.current?.getCurrentTime() && delay !== 0)) {
+      setIsForcingSync(true);
       player?.current?.seekTo(player?.current?.getCurrentTime() + delay);
       setDelay(0);
+      setTimeout(() => setIsForcingSync(false), 1500);
     }
   }, [delay]);
 
+  const toggleSyncing = useCallback(() => setSyncing((sync) => !sync), []);
+
   useEffect(() => {
     let intervalEmit = null;
-    if (isHost) intervalEmit = setInterval(() => handleUpdateTime(), 5000);
+    if (isHost)
+      intervalEmit = setInterval(() => {
+        if (syncing) handleUpdateTime();
+      }, 7000);
     return () => {
       if (intervalEmit) clearInterval(intervalEmit);
     };
@@ -68,27 +77,25 @@ const MediaPlayer = ({
   useEffect(() => {
     let intervalEmit = null;
     if (isHost)
-      intervalEmit = setInterval(() => handleUpdatePlayingStatus(), 5000);
+      intervalEmit = setInterval(() => {
+        if (syncing) handleUpdatePlayingStatus();
+      }, 7000);
     return () => {
       if (intervalEmit) clearInterval(intervalEmit);
     };
-  }, [isHost]);
+  }, [isHost, playingStatus]);
 
   useEffect(() => {
-    if (isHost) handleUpdatePlayingStatus();
-  }, [playingStatus]);
-
-  useEffect(() => {
-    if (!isHost) {
+    if (!isHost && syncing) {
       const delayTime =
         Number(time) - Number(player?.current?.getCurrentTime());
       setDelay((current) => (!isNaN(delayTime) ? delayTime + 0.03 : current));
-      if (Math.abs(delayTime) > 2.5) player?.current?.seekTo(time);
+      if (Math.abs(delayTime) > 7) player?.current?.seekTo(time);
     }
   }, [isHost, time]);
 
   useEffect(() => {
-    if (!isHost) {
+    if (!isHost && syncing && currentPlayingStatus != null) {
       console.log("set currentPlayingStatus", currentPlayingStatus);
       setPlayingStatus(currentPlayingStatus);
     }
@@ -98,47 +105,70 @@ const MediaPlayer = ({
     !isEmpty(fileURL) && (
       <div>
         <div style={backgroundStyle}>
-          <Button
-            type="primary"
-            danger={playingStatus}
-            icon={
-              playingStatus ? <PauseCircleOutlined /> : <PlayCircleOutlined />
-            }
-            onClick={() => setPlayingStatus((status) => !status)}
-          >
-            {playingStatus ? "Dừng video" : "Phát video"}
-          </Button>
-          <span style={{ color: "#3e91f7" }}>{` | `}</span>
+          <div>
+            Host đang mở video:{" "}
+            <b>
+              {currentProgramFromServer
+                ? currentProgramFromServer
+                : "Chưa có host/thông tin"}
+            </b>
+          </div>
+          <div style={{ marginBottom: 8 }}>
+            <span>
+              Trạng thái host:{" "}
+              <b>
+                {currentPlayingStatus == null && "Chưa có host/thông tin"}
+                {currentPlayingStatus === true && "Đang play video"}
+                {currentPlayingStatus === false && "Đang dừng video"}
+              </b>
+              {` | `}
+              Thời gian host đang play:{" "}
+              {time ? secondsToHms(time) : <b>Chưa có host/thông tin</b>}
+            </span>
+          </div>
+
+          <div style={{ marginBottom: 8 }}>
+            <Button
+              type="primary"
+              danger={!playingStatus}
+              icon={
+                playingStatus ? <SyncOutlined spin /> : <PlayCircleOutlined />
+              }
+              onClick={() => setPlayingStatus((status) => !status)}
+            >
+              {playingStatus ? "Đang play video" : "Đang dừng video"}
+            </Button>
+            <span style={{ color: "#3e91f7" }}>{` | `}</span>
+            <Button
+              onClick={toggleSyncing}
+              type="primary"
+              danger={!syncing}
+              icon={syncing ? <SyncOutlined spin /> : <PlayCircleOutlined />}
+            >
+              {isHost && syncing && "Host đang bật đồng bộ"}
+              {isHost && !syncing && "Host đang tắt đồng bộ"}
+              {!isHost && syncing && "Đang đồng bộ từ host"}
+              {!isHost && !syncing && "Đang tắt đồng bộ từ host"}
+            </Button>
+
+            {!isHost && (
+              <>
+                <span style={{ color: "#3e91f7" }}>{` | `}</span>
+                <Button
+                  onClick={debounce(handleForceSync, 2000)}
+                  type="primary"
+                  danger={isForcingSync}
+                >
+                  Đồng bộ lại với host: {delay > 0 && "+"}
+                  {Math.round(delay, 0)}s
+                </Button>
+              </>
+            )}
+          </div>
           <span style={{ fontWeight: "bold", color: "#3e91f7" }}>
             Lật hướng cho video{" "}
           </span>
           <Switch value={flip} onChange={(newFlip) => setFlip(newFlip)} />
-          <div>
-            Host đang mở video:{" "}
-            {currentProgramFromServer ? (
-              <b>{currentProgramFromServer}</b>
-            ) : (
-              "Host chưa mở/Chưa có host"
-            )}
-          </div>
-          {` | `}
-          <span>
-            Trạng thái host:{" "}
-            <b>
-              {currentPlayingStatus ? "Đang play video" : "Đang dừng video"}
-            </b>
-            {` | `}
-            Thời gian host đang play:{" "}
-            {time ? secondsToHms(time) : <b>Chưa có host/thông tin</b>}
-          </span>
-          {!isHost && (
-            <div>
-              <span>Nếu bạn thấy video khác host. Hãy đồng bộ lại </span>
-              <Button onClick={debounce(handleForceSync, 1000)} type="primary">
-                Đồng bộ
-              </Button>
-            </div>
-          )}
         </div>
 
         <ReactPlayer
@@ -151,10 +181,7 @@ const MediaPlayer = ({
           style={flip ? { transform: "scaleX(-1)" } : undefined}
           onStart={() => setPlayingStatus(true)}
           onPlay={() => setPlayingStatus(true)}
-          onPause={() => {
-            console.log("onPause");
-            setPlayingStatus(false);
-          }}
+          onPause={() => setPlayingStatus(false)}
         />
       </div>
     )
